cfc563310fbc5ff2c82ab7d5108025425ec694af
andrejkomissarov <lfr_it@mail.ru>
2023-11-03 14:35:53 +0300

    add hw tasks sem9

---
+++ HW_sem9_task1.py
@@ -0,0 +1,45 @@
+import csv
+import json
+from random import randint
+import math
+
+def save_to_json(func):
+    def wrapper(file_name):
+        with (open(f'{file_name}', 'r', newline='') as f_csv,
+              open('results.json', 'w') as f_json):
+            r = list(csv.reader(f_csv, delimiter=' ', quotechar='|'))
+            roots = []
+            for block in r:
+                abc = list(map(int, block))
+                roots.append(func(*abc))
+            json.dump(roots, f_json)
+    return wrapper
+
+
+def generate_csv_file(file_name, rows):
+    with open(f'{file_name}', 'w', newline='') as f_csv:
+        w = csv.writer(f_csv, delimiter=' ', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+        for _ in range(rows):
+            w.writerow([randint(100, 1000) for _ in range(3)])
+
+@save_to_json
+def find_roots(a, b, c):
+    discr = b ** 2 - 4 * a * c
+    if discr > 0:
+        return [(-b + math.sqrt(discr)) / (2 * a),  (-b - math.sqrt(discr)) / (2 * a)]
+    elif discr == 0:
+        return [-b / (2 * a)]
+
+
+generate_csv_file('input_data.csv', 101)
+find_roots('input_data.csv')
+
+with open('results.json', 'r') as f:
+    data = json.load(f)
+
+if 100 <= len(data) <= 1000:
+    print(True)
+else:
+    print('noooooo')
+
+print(len(data) == 101)
